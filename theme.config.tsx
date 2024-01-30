d0855bd1502405b62c35843ebe880f8dfe16476f
Mohammed Nasim <75156273+nasimstg@users.noreply.github.com>
2024-01-30 19:24:31 +0600

    starting

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,7 +3,7 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>budee.io</span>,
-  docsRepositoryBase: 'https://github.com/nasimstg/budee.io',
+  docsRepositoryBase: 'https://github.com/nasimstg/docs.budee.io/tree/main',
   footer: {
     text: 'budee.io',
   },
