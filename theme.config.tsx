ef75e6af55c8e0cdf91edf967219159844806fc7
Mohammed Nasim <75156273+nasimstg@users.noreply.github.com>
2024-01-30 18:25:51 +0600

    initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>budee.io</span>,
+  docsRepositoryBase: 'https://github.com/nasimstg/budee.io',
+  footer: {
+    text: 'budee.io',
+  },
+}
+
+export default config
